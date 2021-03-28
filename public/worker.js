056dec9be56059566aea86737d9781acb268ee22
Noam Zaks <noamzaks@outlook.com>
2021-03-29 01:32:34 +0300

    Added basic PWA

---
+++ public/worker.js
@@ -0,0 +1,18 @@
+this.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open("Scouting")
+            .then((cache) => {
+                cache.addAll(["index.html", "offline.html"])
+            })
+    )
+})
+
+this.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(() => {
+                return fetch(event.request)
+                    .catch(() => caches.match("offline.html"))
+            })
+    )
+})
