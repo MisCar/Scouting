a1971000680117b92d114dcff3baf191aaadec97
Shahar Ilany <shahar@sps.co.il>
2021-03-29 23:52:47 +0300

    updated scroll issue on webkit browsers

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -26,7 +26,7 @@ const Form: React.FC<Props> = ({ schema }: Props) => {
     if (schema.autonomous === undefined) return <></>
 
     return (
-        <div className="w-full flex flex-col justify-center items-center">
+        <div className="w-full flex flex-col justify-center items-center form-container">
             <InfoCard />
             <Card title="אוטונומי">
                 <CardWidgets prefix="Autonomous" widgets={schema.autonomous} />
