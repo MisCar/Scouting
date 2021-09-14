0be4d0629c2db5de58b270f1a353ad63252f5335
Noam Zaks <imnoamzaks@gmail.com>
2021-09-14 22:11:21 +0300

    Added authentication service and basic functionality with it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core"
+import { AuthenticationService } from "./services/authentication.service"
 
 @Component({
   selector: "app-root",
@@ -7,4 +8,6 @@ import { Component } from "@angular/core"
 })
 export class AppComponent {
   title = "Scouting"
+
+  constructor(public authentication: AuthenticationService) {}
 }
