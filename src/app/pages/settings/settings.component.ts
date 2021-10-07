c85686788ed8eac4834c6e324e89e85d271688ab
Lior Sagy <lior1sagy@gmail.com>
2021-10-07 07:33:04 +0300

    Started get place by touch component

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core"
 
 @Component({
-  selector: 'app-settings',
-  templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.scss']
+  selector: "app-settings",
+  templateUrl: "./settings.component.html",
+  styleUrls: ["./settings.component.scss"],
 })
 export class SettingsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  showCoords(event: any) {
+    let imgae = document.getElementById("img")
+    var x = event.clientX
+    var y = event.clientY
+    var coords = "X coords: " + x + ", Y coords: " + y
+    let position = imgae?.getBoundingClientRect()
+    //let screenHight = $(window).height()
+    //let screenWidth = $(window).width()
+    console.log(coords + " imgae pos " + position?.top)
+    let win = window,
+      doc = document,
+      docElem = doc.documentElement,
+      xScreen = win.innerWidth || docElem.clientWidth,
+      yScrern = win.innerHeight || docElem.clientHeight
+    alert(xScreen + " × " + yScrern)
   }
-
 }
