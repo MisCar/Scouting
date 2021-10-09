05c08d7e4fcaad5e1a28452fa5c0fce540757f50
liorsagy <62811252+liorsagy@users.noreply.github.com>
2021-10-09 12:45:49 +0300

    Added schema editing to admin panel (#5)

--- src/app/pages/admin-panel/admin-panel.component.ts
+++ src/app/pages/admin-panel/admin-panel.component.ts
@@ -1,3 +1,9 @@
+import { Section } from "app/models/schema.model"
+import Schema from "app/models/schema.model"
+import { getDoc } from "@firebase/firestore"
+import { Firestore } from "@angular/fire/firestore"
+import { Component, OnInit } from "@angular/core"
+import { collection, getDocs, setDoc, doc } from "firebase/firestore"
 import { Firestore } from "@angular/fire/firestore"
 import { Component, OnInit } from "@angular/core"
 import { collection, getDocs } from "firebase/firestore"
@@ -5,6 +11,7 @@ import {
   Events,
   TheBlueAllianceService,
 } from "app/services/the-blue-alliance.service"
+import { FormControl } from "@angular/forms"
 
 @Component({
   selector: "app-admin-Panel",
@@ -12,6 +19,11 @@ import {
   styleUrls: ["./admin-Panel.component.scss"],
 })
 export class AdminPanelComponent implements OnInit {
+  async ngOnInit(): Promise<void> {
+    await this.getCurrentData()
+  }
+
+  hideRequiredControl = new FormControl(true)
   ngOnInit(): void {}
 
   events: Events = []
@@ -28,6 +40,10 @@ export class AdminPanelComponent implements OnInit {
 
   showMissingTeams: boolean = true
 
+  sections: Section[] = []
+  schema: Schema = { sections: [] }
+
+
   constructor(
     private firestore: Firestore,
     private tba: TheBlueAllianceService
@@ -39,6 +55,13 @@ export class AdminPanelComponent implements OnInit {
     })
   }
 
+  async getCurrentData() {
+    let schema = await getDoc(doc(this.firestore, "admin/schema1"))
+    let data = schema.data()
+    for (let section of data?.sections) {
+      this.sections.push(section)
+    }
+  }
   async getTeams(game: number): Promise<string[]> {
     await this.tba.getTeams(this.event, this.stage, game).then((val) => {
       this.teams = val
@@ -95,5 +118,59 @@ export class AdminPanelComponent implements OnInit {
 
   findMaxGameNum(gameNumbers: number[]): number {
     return Math.max(...gameNumbers)
+  }
+
+  addSection() {
+    this.sections.push({ title: "", prefix: "", widgets: [] })
+  }
+
+  addWidget(section: Section) {
+    section.widgets.push({ key: "", label: "", type: "Counter" })
+  }
+
+  removeWidget(widgetIndex: number, sectionIndex: number) {
+    this.sections[sectionIndex].widgets.splice(widgetIndex, 1)
+  }
+
+  removeSection(sectionIndex: number) {
+    this.sections.splice(sectionIndex, 1)
+  }
+
+  eventToString(event: Event): string {
+    return (event.target as HTMLInputElement).value
+  }
+
+  eventToNumber(event: Event): number {
+    return Number((event.target as HTMLInputElement).value)
+  }
+
+  async update() {
+    this.schema = { sections: this.sections } as Schema
+    await setDoc(doc(this.firestore, "admin/schema1"), this.schema)
+  }
+
+  moveWidgetUp(widgetIndex: number, sectionIndex: number) {
+    if (widgetIndex == 0) return
+
+    const widget = this.sections[sectionIndex].widgets.splice(widgetIndex, 1)
+    this.sections[sectionIndex].widgets.splice(widgetIndex - 1, 0, widget[0])
+  }
+
+  moveWidgetDown(widgetIndex: number, sectionIndex: number) {
+    if (this.sections[sectionIndex].widgets.length == widgetIndex + 1) return
+    const widget = this.sections[sectionIndex].widgets.splice(widgetIndex, 1)
+    this.sections[sectionIndex].widgets.splice(widgetIndex + 1, 0, widget[0])
+  }
+
+  moveSectionUp(sectionIndex: number) {
+    if (sectionIndex == 0) return
+    const section = this.sections.splice(sectionIndex, 1)
+    this.sections.splice(sectionIndex - 1, 0, section[0])
+  }
+
+  moveSectionDown(sectionIndex: number) {
+    if (sectionIndex == this.sections.length) return
+    const section = this.sections.splice(sectionIndex, 1)
+    this.sections.splice(sectionIndex + 1, 0, section[0])
   }
 }
